27ac6e67b2a3660289b79b9d9c8a04265c83943c
Neptune <eugeneogongo@live.com>
2019-09-29 11:09:49 +0300

    Feat - Log only for Debug

--- apicommunicator/src/main/java/com/openci/apicommunicator/restservices/APIClient.java
+++ apicommunicator/src/main/java/com/openci/apicommunicator/restservices/APIClient.java
@@ -1,35 +1,71 @@
+/*
+ * Created by Eugene on $file.created
+ * Modified on $file.modified
+ * Copyright (c) 2019.
+ */
+
 package com.openci.apicommunicator.restservices;
 
+import com.openci.apicommunicator.BuildConfig;
+
+import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 /**
  * Created by Vicky on 08-01-2018.
+ * Modified by Eugene Odera
+ * Changes
+ * Http Logging of Headers for debugging purposes only
  */
 
-public class APIClient {
-    public static final String PUBLIC_BASE_URL = "https://api.travis-ci.org/";
-    public static final String PRIVATE_BASE_URL = "https://api.travis-ci.com/";
+class APIClient {
+    private static final String PUBLIC_BASE_URL = "https://api.travis-ci.org/";
+    private static final String PRIVATE_BASE_URL = "https://api.travis-ci.com/";
     private static Retrofit publicRetroFit = null;
     private static Retrofit privateRetroFit = null;
 
-    public static Retrofit getPublicClient() {
+
+    static Retrofit getPublicClient() {
         if (publicRetroFit == null) {
             publicRetroFit = new Retrofit.Builder()
                     .baseUrl(PUBLIC_BASE_URL)
+                    .client(getClient())
                     .addConverterFactory(GsonConverterFactory.create())
                     .build();
         }
         return publicRetroFit;
     }
 
-    public static Retrofit getPrivateClient(){
+    static Retrofit getPrivateClient() {
         if (privateRetroFit == null) {
             privateRetroFit = new Retrofit.Builder()
                     .baseUrl(PRIVATE_BASE_URL)
+                    .client(getClient())
                     .addConverterFactory(GsonConverterFactory.create())
                     .build();
         }
         return privateRetroFit;
+    }
+
+    /**
+     * Enables Logging on Debug only
+     *
+     * @return client
+     */
+    private static OkHttpClient getClient() {
+        OkHttpClient.Builder httpClient = new OkHttpClient.Builder();
+        if (BuildConfig.DEBUG) {
+            HttpLoggingInterceptor logging = new HttpLoggingInterceptor();
+
+            //  log level
+            logging.setLevel(HttpLoggingInterceptor.Level.HEADERS);
+
+            // add logging interceptor
+            httpClient.addInterceptor(logging);
+        }
+
+        return httpClient.build();
     }
 }
